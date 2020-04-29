48b35fc8d1ee9ce0f26500a5b90f4a036e740c17
Gitea <gitea@fake.local>
2020-04-29 18:52:12 +0200

    fix missing import

--- elasticsearch-mapping/src/test/java/org/elasticsearch/mapping/ElasticSearchInsertMappingTest.java
+++ elasticsearch-mapping/src/test/java/org/elasticsearch/mapping/ElasticSearchInsertMappingTest.java
@@ -24,6 +24,7 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
